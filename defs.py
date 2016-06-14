b8ca3321b407a49fd85850ef13da65781d88a5b9
evgeny <Evgeny Efimenko>
2016-06-14 15:38:14 +0300

    Updated parameters setting

--- defs.py
+++ defs.py
@@ -1,8 +1,12 @@
 import math
 
 # common constants
-COARSE_GRID_SIZE = 1024
-PML_SIZE = 12
+resolution = 32 # steps per wavelength
+courant = 3 # courant number dx/c dt
+COARSE_GRID_SIZE = 32 * resolution # 32 wavelengths
+FINE_GRID_SIZE = 8*resolution # in coarse grid cell units
+N0 = 20 # number of periods in pulse
+PML_SIZE = 16
 C =  2.99792458e+10
 PI = 3.14159265358979
 EM = 0.910938215e-27
@@ -11,16 +15,17 @@ E = -4.80320427e-10
 wavelength = 0.8e-4
 W0 = 2*PI*C/wavelength
 REL_FIELD = -2*PI*EM*C*C/(E*wavelength)
-dx = wavelength / 32
-dt = dx/(4*C)
+
+
+dx = wavelength / resolution
+dt = dx/(courant*C)
 x0 = -COARSE_GRID_SIZE * dx / 2
 
-FINE_GRID_SIZE = 256 # in coarse grid cell units
 DEBUG_PADDING = 2
 AUX_GRID_SIZE = 1
 
-ITERATIONS = 5000
-OUTPUT_PERIOD = 200
+OUTPUT_PERIOD = resolution * int(courant)
+ITERATIONS = 40*OUTPUT_PERIOD + 1
 
 # pulse description
 def sgn(x):
@@ -30,10 +35,10 @@ def block(x, xmin, xmax):
     return (sgn(x - xmin) + sgn(xmax - x)) * 0.5
 
 def f(t):
-    return math.sin(W0*t/20)
+    return math.sin(W0*t/N0)
 
 def form(t):
-    return f(t)*f(t)*f(t*20)*block(t, 0, 20*PI/W0)
+    return f(t)*f(t)*f(t*N0)*block(t, 0, N0*PI/W0)
 
 DELAY = 16 * wavelength / C
 
